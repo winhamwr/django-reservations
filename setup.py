93c05bc75eaeab393f5d03a28e5b1c40685981da
Wes Winham <winhamwr@gmail.com>
2009-07-06 00:00:33 -0400

    Added initial django app structure and tweaked setup.py properties

--- setup.py
+++ setup.py
@@ -1,7 +1,32 @@
 import os
-if os.path.exists("paver-minilib.zip"):
-    import sys
-    sys.path.insert(0, "paver-minilib.zip")
-
-import paver.tasks
-paver.tasks.main()
+from distutils.core import setup
+ 
+def find_packages(root):
+    # so we don't depend on setuptools; from the Storm ORM setup.py
+    packages = []
+    for directory, subdirectories, files in os.walk(root):
+        if '__init__.py' in files:
+            packages.append(directory.replace(os.sep, '.'))
+    return packages
+ 
+setup(
+    name = 'django-reservations',
+    version = '0.1dev',
+    description = 'Reservation management for events and resources',
+    author = 'Wes Winham',
+    author_email = 'winhamwr@gmail.com',
+    license = 'BSD',
+    url = 'http://github.com/winhamwr/django-reservation',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        ],
+    packages = find_packages('reservation'),
+)
