5507eae9564626561de74b961c61b52ed69d75fb
winhamwr <winhamwr@gmail.com>
2009-10-29 04:45:30 -0400

    Modified the data model to be simpler and more in line with django_attendance.

--- django_reservations/models.py
+++ django_reservations/models.py
@@ -1,28 +1,47 @@
-from django.contrib.contenttypes import generic
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.contrib.auth.models import User
+from django.contrib import admin
 
-reservation_statuses = (("ATTENDING", _("Attending")),
-                        ("NOT ATTENDING", _("Not Attending")),
-                        ("UNSURE", _("Unsure")),)
+from schedule.models import Occurrence
 
-class Reservation(models.Model):
-    '''
-    The reservation status of a user for an event.
-    '''
-    event = generic.GenericForeignKey('event_content_type', 'event_object_id')
-    event_content_type = models.ForeignKey(ContentType)
-    event_object_id = models.IntegerField()
-    user = models.ForeignKey(User)
-    status = models.CharField(_("reservation status"),
-                              choices=reservation_statuses, default=None,
-                              null=True, blank=True, max_length=20)
+class EventReservations(models.Model):
+    """
+    Reservation settings for an occurrence.
+    """
+    occurrence = models.OneToOneField(Occurrence)
+    reservation_limit = models.IntegerField(default=0) # 0 is no limit
+    reservations = models.ManyToManyField(User)
+
 
     class Meta:
-        verbose_name = _('reservation')
-        verbose_name_plural = _('reservations')
+        verbose_name = _('reservation setting')
+        verbose_name_plural = _('reservation settings')
 
     def __unicode__(self):
-        return "%s reservation for %s" % (self.event, self.user)
+        return "reservation settings for %s" % self.occurrence
+
+    def user_reserved(self, user):
+        """
+        Determine whether a user has confirmed yes for this occurrence.
+        """
+        count = self.reservations.filter(user=user).count()
+        if count > 0:
+            return True
+        else:
+            return False
+
+    def can_rsvp(self):
+        """
+        Can another user RSVP yes or are we at our reservation_limit?
+        """
+        if self.reservation_limit <= 0:
+            return True
+
+        count = self.reservations.count()
+        if count >= self.reservation_limit:
+            return False
+
+        return True
+
+admin.site.register(EventRegistration)
